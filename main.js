5c7312dd20357333249a85a5217e6528f970c8f2
Arunabh Saikia <developer.ariyanas@gmail.com>
2020-08-25 04:08:40 +0530

    Added support for angular with electron

---
+++ main.js
@@ -0,0 +1,49 @@
+const { app, BrowserWindow } = require("electron");
+
+const url = require("url");
+const path = require("path");
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    backgroundColor: "#ffffff",
+    icon: `file://${__dirname}/dist/assets/logo.png`,
+    webPreferences: {
+      webSecurity: true,
+    },
+  });
+
+  const fileUrl = url.format({
+    pathname: path.join(__dirname, `/dist/angular-electron/index.html`),
+    protocol: "file:",
+    slashes: true,
+  });
+
+  console.log(fileUrl);
+
+  // win.loadURL(`file://${__dirname}/dist/index.html`)
+  win.loadURL(fileUrl);
+
+  win.webContents.openDevTools();
+
+  win.on("closed", function () {
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  if (win === null) {
+    createWindow();
+  }
+});
